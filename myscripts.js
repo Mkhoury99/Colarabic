c7924ee66627ad6f6472b2d26c6a0b9d96c888be
Mkhoury99 <109634447+Mkhoury99@users.noreply.github.com>
2022-08-09 01:42:07 -0700

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -32,7 +32,7 @@ function isHarake(c) {
         text1=text[i] ;
         i=i+1;
       }
-      else if ((text.charAt(i + 3) == "ل" && text.charAt(i + 2) == "ا" && text.charAt(i) == "ب"  && isHarake(text.charAt(i+1)))  && text.charAt(i+1)!= " " && text.charAt(i+2)!= " " ) {
+      else if ((text.charAt(i + 3) == "ل" && text.charAt(i + 2) == "ا" && (text.charAt(i) == "ب"  ) && isHarake(text.charAt(i+1)))  && text.charAt(i+1)!= " " && text.charAt(i+2)!= " " ) {
             text1 = text[i] + text[i + 1] + text[i + 2] + text[i+3];
             i = i + 4;
           
@@ -84,6 +84,17 @@ function isHarake(c) {
             i = i + 4;
         }  
         else if ((isHarake(text.charAt(i+1)) && (text.charAt(i + 2) == "و" || (text.charAt(i + 2) == "ا" && text.charAt(i+3)!= "ل" )|| text.charAt(i + 2) == "ي" || text.charAt(i+2)=='ى')) ||   (!isHarake(text.charAt(i+1)) && (text.charAt(i + 1) == "و" || text.charAt(i + 1) == "ا"  || text.charAt(i + 1) == "ي" || text.charAt(i+1)=='ى')) ) {
+            if((isHarake(text.charAt(i+1)) && text.charAt(i + 3) == "و" || text.charAt(i + 3) == "ا"|| text.charAt(i + 3) == "ي" || text.charAt(i+3)=='ى' ) || (!isHarake(text.charAt(i+1)) && text.charAt(i + 2) == "و" || text.charAt(i + 2) == "ا"|| text.charAt(i + 2) == "ي" || text.charAt(i+2)=='ى' ))
+            {
+              text1=text[i];
+              if(isHarake(text.charAt(i + 1))){
+                text1+=text[i+1];
+                i = i + 2;
+              }
+              else 
+                i = i + 1;
+            }
+            else{
             text1 = text[i] + text[i + 1];
             if(isHarake(text.charAt(i + 1)))
                 {
@@ -106,6 +117,7 @@ function isHarake(c) {
                 text1+=text[i];
                 i+=1;
               }
+             }
              }
 
         } 
@@ -156,25 +168,55 @@ function isHarake(c) {
 }
 
 function hideDisplay() {
+  sizeFunction();
   var x = document.getElementById("originalText");
   var y = document.getElementById("coloredText");
   var z = document.getElementById("colorButton");
   var w =document.getElementById("backButton");
+  var clBtn = document.getElementById("clearButton");
+  //var ui = document.getElementById("cloud");
   if (x.style.display === "none") {
     x.style.display = "block";   
     z.style.display = "block";
+    clBtn.style.display = "block";
     w.style.display ="none";
     w.style.visibility="hidden";
     y.innerHTML = '';
     y.style.display ="none";
     y.style.visibility ="hidden";
+    //ui.style.visibility = "visible";
 
   } else {
     x.style.display = "none";
     z.style.display="none";
+    clBtn.style.display="none";
     w.style.display="block";
     y.style.display ="block";
     y.style.visibility ="visible";
     w.style.visibility ="visible";
+    //ui.style.visibility = "hidden";
   }
+}
+
+function sizeFunction(){
+var w = window.innerWidth;
+var x = document.getElementById("backButton");
+var y = document.getElementById("logo");
+if(w<900){
+ x.style.marginRight="160px";
+ x.style.marginTop="50px";
+ x.style.position="static";
+ y.style.zIndex = "-1";
+}
+else {
+  x.style.position="fixed";
+x.style.marginRight="50px";
+  y.style.zIndex = "0";
+  x.style.marginTop="0px";
+}
+}
+
+
+function clearText(){
+ return  document.getElementById("originalText").value='';
 }
